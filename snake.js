85608ece0d0ec6154bd6bd4cdf0bffae6cac874f
neriashitrit <80205367+neriashitrit@users.noreply.github.com>
2021-03-11 03:08:46 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,1 +1,23 @@
+let snakePart = {x: 10,y:10}
+let snakeBody = {snakePart}
+//const Board = document.getElementById('board');
+
+/*function graw(snakePart){
+snakeBody.unshift(snakePart);
+draw(Board);
+}*/
+
+
+
+export function drawSnake(Board)
+{
+    console.log("in drawsnake");
+    const oneSnakePart = document.createElement('div')
+    oneSnakePart.style.gridColumnStart = snakePart.x
+    oneSnakePart.style.gridRowStart = snakePart.y
+    oneSnakePart.classList.add('snakepiece')
+    Board.appendChild (oneSnakePart)
+    console.log("end drawsnake");
+
+}
 
