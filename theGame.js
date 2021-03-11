4edb7811c50a05b74f800dc3e04fd242e5437361
neriashitrit <80205367+neriashitrit@users.noreply.github.com>
2021-03-11 03:09:38 +0200

    Create theGame.js

---
+++ theGame.js
@@ -0,0 +1,52 @@
+import{drawSnake}from './snake.js'
+const Board = document.getElementById('board');
+
+let howManyLives = 3
+let secondsPassed;
+let oldTimeStamp = 0;
+
+
+function init (timeStamp)
+{
+    if (howManyLives==0)
+      {
+        if (confirm('Game over. Press ok to restart.')) {
+          window.location = '/'
+        }
+        return
+      } 
+
+   window.requestAnimationFrame(init);
+   secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+   
+    if (secondsPassed<1)return;
+    oldTimeStamp = timeStamp;
+   
+    //update()
+    draw()
+    
+    
+
+}
+
+window.requestAnimationFrame(init);
+
+function update(){
+    updateSnake()
+    updateFood()
+    if(outsideGrid() || snakeIntersection())
+    {
+        howManyLives-=1
+        emptySnake()
+    }
+}
+
+function draw()
+{
+Board.innerHTML = ''
+drawSnake(Board)
+//drawFood()
+
+
+
+}
