af6a35a9c71ead4313984c89c05230eb57748e0a
neriashitrit <80205367+neriashitrit@users.noreply.github.com>
2021-03-16 11:57:37 +0200

    Update eventHandler.js
    initDirection

--- eventHandler.js
+++ eventHandler.js
@@ -4,9 +4,9 @@ window.addEventListener('keydown', logKey);
 
 
 function logKey(e) {
-  if((e.code == 'ArrowUp')&&(Lastdirection.y==0))    {direction = { x: 0, y: -1}};
+  if((e.code == 'ArrowUp')&&(Lastdirection.y==0))    {direction = { x: 0, y: -1}}; //If the user presses up or down and the currently movement is currently horizontal
   if((e.code == 'ArrowDown')&&(Lastdirection.y==0))  {direction = { x: 0, y: 1 }};
-  if((e.code == 'ArrowRight')&&(Lastdirection.x==0)) {direction = { x: 1, y: 0 }};
+  if((e.code == 'ArrowRight')&&(Lastdirection.x==0)) {direction = { x: 1, y: 0 }};//If the user presses up or down and the currently movement is currently vertical
   if((e.code == 'ArrowLeft') &&(Lastdirection.x==0)) {direction = { x: -1, y: 0}};
 }
 
@@ -15,3 +15,6 @@ export function getDirection(){
     return  direction;
 }
 
+export function initDirection(){//for firset move
+    direction = { x: 0, y: 0}
+}
