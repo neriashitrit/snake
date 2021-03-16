687351c9853be8d890a935e90f0f63fd1073e3e8
neriashitrit <80205367+neriashitrit@users.noreply.github.com>
2021-03-16 11:58:33 +0200

    Update food.js
    bad position if the food is on the snake

--- food.js
+++ food.js
@@ -1,21 +1,22 @@
-const food =  {x:0,y:0}
+import{getHead,inSnakeBody}from './snake.js'
+
+let food =  {x:0,y:0}
 const gridSize = 21;
 updateFood();
 
 
 
-export function updateFood(){
-    console.log("in updateFood");
-    console.log(food.x ,food.y);
+export function updateFood(){//Chooses a random location for food. If it's on the snake choose an alternate location
 food.x = Math.floor((Math.random()*gridSize)+1);  
 food.y = Math.floor((Math.random()*gridSize)+1);
-console.log(food.x ,food.y);
+    if(badPosition(food)){
+        updateFood();
+    }
 }
 
 
 export function drawFood(Board)
 {
-    console.log("in drawfood");
     const foodPiece = document.createElement('div')
     foodPiece.style.gridColumnStart = food.x
     foodPiece.style.gridRowStart = food.y
@@ -26,3 +27,7 @@ export function drawFood(Board)
 export function getFood(){
 return food;
 }
+ function badPosition(food){ //Uses a function that checks if the snake's head has hit its body, and adds the head for the testing
+     if (food==getHead()|| inSnakeBody(food))
+     return true 
+ }
